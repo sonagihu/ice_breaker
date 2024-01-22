399e921189fa8d92fa73bae0bc1bb07f7d18955d
sonagihu <sonagihu@gmail.com>
2024-01-22 10:36:41 +0900

    function ice_break

--- ice_breaker_aftertwitter.py
+++ ice_breaker_aftertwitter.py
@@ -10,7 +10,8 @@ from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from urllib.parse import urlparse
 
 name="Elon Musk"
-def ice_break(name: str) -> str:
+if __name__ == "__main__":
+    print("Hello Langchain")
     
     linkedin_profile_url =linkedin_lookup_agent(name="Eden Marco Udemy") 
     print("linkedin_url found: {0}".format(linkedin_profile_url))
@@ -18,7 +19,14 @@ def ice_break(name: str) -> str:
     print("linkedin_url redirect : {0}".format(linkedin_profile_url))
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)    
 
+    # 강의에서는 agent가 찾은 url에서 name을 가져오기 위해 시도하다가
+    # 알아서 찾는다고 하는데
+    # 나는 계속 반복하다가 에러가 남
+    # 그래서 uri를 가져오게 수정하고
+    # 가져온 uri를 내가 짜름
     twitter_username = urlparse(twitter_lookup_agent(name=name)).path[1:]
+     
+    # 여기도 gist에서   
     tweets = scrape_user_tweets(username=twitter_username, num_tweets=100)
         
     summary_template = """
@@ -29,19 +37,11 @@ def ice_break(name: str) -> str:
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], 
-        template=summary_template
+        input_variables=["information"], template=summary_template
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     
-    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
-    print(result)
-    
-    return result
-    
-if __name__ == "__main__":
-    print("Hello Langchain")
-    result = ice_break(name="Harrison Chase")
+    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
